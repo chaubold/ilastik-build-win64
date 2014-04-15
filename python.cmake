7f17b4fee9f44320c99ea9126982f48ef262ec33
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2014-04-15 11:09:04 +0200

    added option WITH_OPENSSL

--- python.cmake
+++ python.cmake
@@ -16,7 +16,25 @@ include (bzip2)
 
 # include (zlib)
 include (nasm)
-include (openssl)
+
+if(${ILASTIK_BITNESS} STREQUAL "32")
+    set(PYTHON_BITNESS "Win32")
+    set(PYTHON_PATH_PREFIX ".")
+else()
+    set(PYTHON_BITNESS "x64")
+    set(PYTHON_PATH_PREFIX "amd64")
+endif()
+
+if(WITH_OPENSSL)
+    include (openssl)
+    set(OPENSSL_DEPENDS ${openssl_NAME})
+    set(OPENSSL_CONFIG perl -p -i.bak -e s/openssl-0.9.8l/${openssl_NAME}/g pyproject.props)
+    set(OPENSSL_BUILD devenv PCbuild.sln /build "Release|${PYTHON_BITNESS}" /project _ssl)
+else()
+    set(OPENSSL_DEPENDS "")
+    set(OPENSSL_CONFIG "")
+    set(OPENSSL_BUILD "")
+endif()
 
 external_source (python
     2.7.3
@@ -27,15 +45,6 @@ external_source (python
 
 SET(PYTHON_PREFIX ${ILASTIK_DEPENDENCY_DIR}/python)
 SET(PYTHON_BIN_DIR ${python_SRC_DIR}/PCbuild)
-
-if(${ILASTIK_BITNESS} STREQUAL "32")
-    set(PYTHON_BITNESS "Win32")
-    set(PYTHON_PATH_PREFIX ".")
-else()
-    set(PYTHON_BITNESS "x64")
-    set(PYTHON_PATH_PREFIX "amd64")
-endif()
-
 
 # Add missing '/MANIFEST' compiler flag to msvc9compiler.py
 set(python_PATCH ${PYTHON_PATH_PREFIX}\\python.exe ${PROJECT_SOURCE_DIR}/patches/patch_python.py ../Lib/distutils/msvc9compiler.py ../Lib/distutils/cygwinccompiler.py)
@@ -49,7 +58,7 @@ FILE(APPEND  ${python_INSTALL} "file(INSTALL ../Lib DESTINATION ${PYTHON_PREFIX}
 
 message ("Installing ${python_NAME} into ilastik build area: ${ILASTIK_DEPENDENCY_DIR} ...")
 ExternalProject_Add(${python_NAME}
-    DEPENDS             ${bzip2_NAME} ${openssl_NAME} ${nasm_NAME} # ${zlib_NAME}
+    DEPENDS             ${bzip2_NAME} ${OPENSSL_DEPENDS} ${nasm_NAME} # ${zlib_NAME}
     PREFIX              ${ILASTIK_DEPENDENCY_DIR}
     URL                 ${python_URL}
     URL_MD5             ${python_MD5}
@@ -59,7 +68,7 @@ ExternalProject_Add(${python_NAME}
     CONFIGURE_COMMAND   devenv PCbuild.sln /upgrade
     BUILD_COMMAND       ${ADD_PATH} "${PERL_PATH}" "${nasm_PATH}"
                         \nset HOST_PYTHON=${PYTHON_PATH_PREFIX}/python.exe
-                        \nperl -p -i.bak -e s/openssl-0.9.8l/${openssl_NAME}/g pyproject.props
+                        \n ${OPENSSL_CONFIG}
                         \n ${PATCH_EXE} -i ${PROJECT_SOURCE_DIR}/patches/build_ssl.py.patch
                         \ndevenv PCbuild.sln /build "Release|${PYTHON_BITNESS}" /project Python
                         \ndevenv PCbuild.sln /build "Release|${PYTHON_BITNESS}" /project _ctypes
@@ -70,7 +79,7 @@ ExternalProject_Add(${python_NAME}
                         \ndevenv PCbuild.sln /build "Release|${PYTHON_BITNESS}" /project select
                         \ndevenv PCbuild.sln /build "Release|${PYTHON_BITNESS}" /project unicodedata
                         \ndevenv PCbuild.sln /build "Release|${PYTHON_BITNESS}" /project bz2
-                        \ndevenv PCbuild.sln /build "Release|${PYTHON_BITNESS}" /project _ssl
+                        \n ${OPENSSL_BUILD}
                         \n ${python_PATCH}
     INSTALL_COMMAND     ${CMAKE_COMMAND} -P ${python_INSTALL}
 )
