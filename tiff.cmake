0324d48137fbcc403cbda21a1d226400e992cceb
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2014-05-22 20:32:38 +0200

    Revert "upgraded to libtiff 4.1": TIFF export functions seem to be unable to overwrite existing files.
    This reverts commit 77c90f29c4208149776702c8f4002ece723931b9.

--- tiff.cmake
+++ tiff.cmake
@@ -11,10 +11,10 @@ include (ExternalSource)
 include (python)
 
 external_source (tiff
-    4.1
-    libtiff-4.1_only.zip
-    ce361b4e1abca2456f1d05614992d953
-    http://bigtiff.org
+    4.0.1
+    tiff-4.0.1.tar.gz
+    fae149cc9da35c598d8be897826dfc63
+    http://download.osgeo.org/libtiff
     FORCE)
 
 # insert zlib and jpeg path into nmake.opt
@@ -24,7 +24,7 @@ set (tiff_PATCH ${PYTHON_EXE} ${PROJECT_SOURCE_DIR}/patches/patch_tiff.py ${tiff
 SET(tiff_INSTALL ${ILASTIK_DEPENDENCY_DIR}/tmp/tiff_install.cmake)
 FILE(WRITE   ${tiff_INSTALL} "file(INSTALL libtiff/tiff.h libtiff/tiffconf.h libtiff/tiffio.h libtiff/tiffiop.h libtiff/tiffvers.h DESTINATION ${ILASTIK_DEPENDENCY_DIR}/include)\n")
 FILE(APPEND  ${tiff_INSTALL} "file(INSTALL libtiff/libtiff.dll DESTINATION ${ILASTIK_DEPENDENCY_DIR}/bin)\n")
-FILE(APPEND  ${tiff_INSTALL} "file(INSTALL libtiff/libtiff_i.lib DESTINATION ${ILASTIK_DEPENDENCY_DIR}/lib)\n")
+FILE(APPEND  ${tiff_INSTALL} "file(INSTALL libtiff/libtiff_i.lib libtiff/libtiff.lib DESTINATION ${ILASTIK_DEPENDENCY_DIR}/lib)\n")
 
         
 message ("Installing ${tiff_NAME} into ilastik build area: ${ILASTIK_DEPENDENCY_DIR} ...")
@@ -34,11 +34,9 @@ ExternalProject_Add(${tiff_NAME}
     URL                 ${tiff_URL}
     URL_MD5             ${tiff_MD5}
     UPDATE_COMMAND      ""
-    PATCH_COMMAND       ${PATCH_EXE} -p0 -i ${PROJECT_SOURCE_DIR}/patches/tiff.patch
-                     \n ${tiff_PATCH}
+    PATCH_COMMAND       ${tiff_PATCH}
     CONFIGURE_COMMAND   ""
-    BUILD_COMMAND       cd libtiff
-                     \n nmake /f Makefile.vc
+    BUILD_COMMAND       nmake /f Makefile.vc
     BUILD_IN_SOURCE     1
     INSTALL_COMMAND     ${CMAKE_COMMAND} -P ${tiff_INSTALL}
 )
